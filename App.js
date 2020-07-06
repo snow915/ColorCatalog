a0ccb603cc708f4c2d9e0486a7fe72548155899f
snow915 <cris_ganor@hotmail.com>
2020-07-05 19:33:39 -0500

    add: some native APIS

--- App.js
+++ App.js
@@ -1,18 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
+import {StatusBar} from 'expo-status-bar';
 import React from 'react';
-import {  View, ActivityIndicator, ProgressBarAndroid, Button } from 'react-native';
+import {
+    Text,
+    View,
+    ActivityIndicator,
+    ProgressBarAndroid,
+    Button,
+    Alert,
+    Dimensions,
+    Platform
+} from 'react-native';
+
+const {height, width} = Dimensions.get("window") //Get the dimensions of the screen
 
 export default function App() {
 
-  const onButtonPress = () => {
-    console.log(`${new Date().toLocaleDateString()} button press`)
-  }
+    const onButtonPress = () => {
+        Alert.alert(`${new Date().toLocaleTimeString()} button press`)
+    }
 
-  return (
-    <View style={{padding: 50}}>
-      <ActivityIndicator size="large" color="#61DBFB"/>
-      <ProgressBarAndroid progress={0.5}/>
-      <Button title="Click me" onPress={onButtonPress}/>
-    </View>
-  );
+    return (
+        <View style={{padding: 50}}>
+          {Platform.OS === "ios" && <ActivityIndicator size="large" color="#61DBFB"/>}
+          {Platform.OS === "android" && <ProgressBarAndroid
+              styleAttribute="Horizontal"
+              indeterminate={false}
+              progress={0.5}
+
+          />}
+            <Button title="Click me" onPress={onButtonPress}/>
+            <Text>Height: {height}</Text>
+          <Text>Width: {width}</Text>
+            <Text>OS: {Platform.OS}</Text>
+        </View>
+    );
 }

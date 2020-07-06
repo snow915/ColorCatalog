93fadc7bdc142272ed6f9b1023e2f72969e52aea
snow915 <cris_ganor@hotmail.com>
2020-07-05 19:42:20 -0500

    add: new style with stylesheet and remove some components, only are texts

--- App.js
+++ App.js
@@ -1,37 +1,37 @@
 import {StatusBar} from 'expo-status-bar';
 import React from 'react';
 import {
+    StyleSheet,
     Text,
     View,
-    ActivityIndicator,
-    ProgressBarAndroid,
-    Button,
-    Alert,
-    Dimensions,
-    Platform
 } from 'react-native';
 
-const {height, width} = Dimensions.get("window") //Get the dimensions of the screen
 
 export default function App() {
 
-    const onButtonPress = () => {
-        Alert.alert(`${new Date().toLocaleTimeString()} button press`)
-    }
-
     return (
-        <View style={{padding: 50}}>
-          {Platform.OS === "ios" && <ActivityIndicator size="large" color="#61DBFB"/>}
-          {Platform.OS === "android" && <ProgressBarAndroid
-              styleAttribute="Horizontal"
-              indeterminate={false}
-              progress={0.5}
-
-          />}
-            <Button title="Click me" onPress={onButtonPress}/>
-            <Text>Height: {height}</Text>
-          <Text>Width: {width}</Text>
-            <Text>OS: {Platform.OS}</Text>
+        <View style={styles.page}>
+          <Text style={styles.text}>Red</Text>
+          <Text style={[styles.text, styles.selectedText]}>Green</Text>
+          <Text style={styles.text}>Blue</Text>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+  page : {
+    marginTop : 40,
+    backgroundColor : "#DDD"
+  },
+  text: {
+    fontSize : 22,
+    color: "red",
+    backgroundColor: "yellow",
+    margin: 10,
+    padding: 5
+  },
+  selectedText: {
+    backgroundColor: "red",
+    color: "yellow",
+  }
+});

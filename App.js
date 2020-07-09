3448ed6a1b1325b1196d7ac934667adeab6c1665
snow915 <cris_ganor@hotmail.com>
2020-07-08 22:53:07 -0500

    add: function in addColor for add colors dinamicaly, in lines 15-19

--- App.js
+++ App.js
@@ -1,21 +1,27 @@
 import React, {useState} from 'react';
 import {
     StyleSheet,
-    Alert,
     FlatList
 } from 'react-native';
 import {getBackgroundColor} from "react-native/Libraries/LogBox/UI/LogBoxStyle";
 import ColorButton from "./components/ColorButton";
-import defaultColors from "./data/defaultColors.json"
 import ColorForm from "./components/ColorForm";
+import {generate} from "shortid"; // This will generate a unique ID (npm i shortid)
+
+//Function for adding colors
 
 export default function App() {
     const [backgroundColor, setBackgroundColor] = useState("white")
+    const [colors, setColors] = useState([]);
+    const addColor = color => {
+        const newColor = {id: generate(), color}
+        setColors([newColor, ...colors]); // ...colors for add the rest of the color at the end of the array
+    };
     return (
         <>
-            <ColorForm onNewColor={newColor => Alert.alert(`TODO: add color ${newColor}`)}/>
+            <ColorForm onNewColor={addColor}/>
             <FlatList style={[styles.container, {backgroundColor}]}
-                      data={defaultColors}
+                      data={colors}
                       renderItem={({item}) => {
                           return (
                               <ColorButton key={item.id} backgroundColor={item.color} onPress={setBackgroundColor}/>

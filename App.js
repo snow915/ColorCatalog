f27d7fafd8dc5c9f53b6bf4190c1929ca6f7cd80
snow915 <cris_ganor@hotmail.com>
2020-07-08 22:59:22 -0500

    add: a hook called useColors for separate the functionability from the presentation

--- App.js
+++ App.js
@@ -8,15 +8,19 @@ import ColorButton from "./components/ColorButton";
 import ColorForm from "./components/ColorForm";
 import {generate} from "shortid"; // This will generate a unique ID (npm i shortid)
 
-//Function for adding colors
-
-export default function App() {
-    const [backgroundColor, setBackgroundColor] = useState("white")
+//Hooks separate functionality from the presentation
+const useColors = () => {
     const [colors, setColors] = useState([]);
     const addColor = color => {
         const newColor = {id: generate(), color}
         setColors([newColor, ...colors]); // ...colors for add the rest of the color at the end of the array
     };
+    return {colors, addColor} //Expose the list of colors in the addColor function to the consumer of this hook
+}
+
+export default function App() {
+    const [backgroundColor, setBackgroundColor] = useState("white")
+    const {colors, addColor} = useColors(); //Capture colors in the addColor function from my useColors hook
     return (
         <>
             <ColorForm onNewColor={addColor}/>
